b4561578152f035e919c23da12874b0d7bf9bf01
PapaDaCodr <131633759+PapaDaCodr@users.noreply.github.com>
2024-10-12 00:04:46 +0000

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_KEY = process.env.PIXABAY_API_KEY;
+const API_KEY = process.env.PIXABAY_API_KEY!;
 const BASE_URL = 'https://pixabay.com/api/';
 
 export interface Wallpaper {
